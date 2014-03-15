3959ec82057d5d89d9043ac8aeb6ffa2825b6b7d
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-03-16 01:24:14 +0900

    rails g scaffold pages name:string content:text title:string

---
+++ db/migrate/20140315162341_create_pages.rb
@@ -0,0 +1,11 @@
+class CreatePages < ActiveRecord::Migration
+  def change
+    create_table :pages do |t|
+      t.string :name
+      t.text :content
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
