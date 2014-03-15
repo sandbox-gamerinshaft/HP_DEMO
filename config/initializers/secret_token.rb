fd9e6042c8d90799a2b876c4a4a93f41671b9072
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-03-16 01:08:34 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RikkyouHp::Application.config.secret_token = '7feb25e89a7ac46a3570398b4b4091a8452b6a7672763a6de6215680a58011a90b6cc5f26d66bb65fb40ad05e463e3e3ae0677ed3094bbd867c07995f7cf1381'
